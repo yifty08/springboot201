20da8fd07492cb81081fce90212edda4e9339c44
yifty08 <yifty08@icloud.com>
2019-03-11 16:50:16 -0400

    final

---
+++ src/test/java/com/example/springboot201/Springboot201ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springboot201;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot201ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
